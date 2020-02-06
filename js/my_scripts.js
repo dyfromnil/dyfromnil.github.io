0722df8ed4cd7ca3657f4b0bee1bb1468677e9b5
DY <dywechat@163.com>
2020-02-06 10:14:13 +0800

    Site updated: 2020-02-06 10:14:12

--- js/my_scripts.js
+++
@@ -1,6 +0,0 @@
-$(".column-left,.column-right").mCustomScrollbar({
-    axis: "y",
-    setHeight: "97vh",
-    scrollInertia: 100,
-    autoHideScrollbar: true
-});
---
+++ js/scroll-bar.js
@@ -0,0 +1,8 @@
+if($(window).width() >= 769){
+    $(".column-left,.column-right").mCustomScrollbar({
+        axis: "y",
+        setHeight: "97vh",
+        scrollInertia: 100,
+        autoHideScrollbar: true
+    });
+}
